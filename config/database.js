681ea1596bb8da9c732c8c6a380f9ef84a53039d
Gabriel Garcia <gabrielgarciasp@gmail.com>
2020-03-12 21:32:43 -0300

    Database production

--- config/database.js
+++ config/database.js
@@ -1,23 +1,23 @@
 const mysql = require('mysql2/promise');
 
-// const pool = mysql.createPool({
-//     host: 'remotemysql.com',
-//     user: '2A4TOECQbn',
-//     database: '2A4TOECQbn',
-//     password: 'GNeQT9nYLh',
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0,
-// });
-
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'doghma',
-    password: '',
+    host: 'remotemysql.com',
+    user: '2A4TOECQbn',
+    database: '2A4TOECQbn',
+    password: 'GNeQT9nYLh',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
 });
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'doghma',
+//     password: '',
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0,
+// });
 
 module.exports = pool;
